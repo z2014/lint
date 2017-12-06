926c894efedc277a2b7f54760578a13847c5a3bb
z2014 <591763389@qq.com>
2017-12-06 10:25:00 +0800

    add dev

--- config.js
+++ config.js
@@ -7,6 +7,7 @@ module.exports = {
             "eslint-plugin-import": "2.2.0",
             "eslint-plugin-lean-imports": "0.3.3",
             "eslint-plugin-react": "6.10.3",
+            "eslint-plugin-jsx-a11y": "4.0.0",
             "stylelint": "7.12.0",
             "stylelint-config-standard": "16.0.0",
             "stylelint-processor-html": "1.0.0"
